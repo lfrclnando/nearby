ad725ed5cdba6c410fb4fcb3cde8e72ba98434e1
“lfrclnando” <lfernando.div@gmail.com>
2024-12-14 20:51:13 -0300

    Feat/Creating components categories & category + updates for home.

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
+
+import { api } from "@/services/api";
+
+import { Categories, CategoriesProps } from "@/components/categories";
 
 export default function Home() {
+  const [categories, setCategories] = useState<CategoriesProps>([])
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories")
+      setCategories(data)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.")
+    }
+  }
+  useEffect(() => {
+    fetchCategories()
+  }, [])
   return(
-    <View style={{ flex: 1 }}>
-      
-    </View>
-  )
+  <View style={{ flex: 1 }}>
+    <Categories data={categories} />
+  </View>)
 }
---
+++ src/components/categories/index.tsx
@@ -0,0 +1,26 @@
+import { FlatList } from "react-native";
+import { s } from "./styles";
+import { Category } from "../category";
+
+export type CategoriesProps = {
+  id: string
+  name: string
+}[]
+
+type Props = {
+  data: CategoriesProps
+}
+
+export function Categories({ data }: Props) {
+  return (
+    <FlatList 
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <Category name={item.name} iconId={item.id} />}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={s.content}
+      style={s.container}
+    />
+  )
+}
