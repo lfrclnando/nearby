73682caf115d0dcd7b53e0eca05a4edc7c17aa52
“lfrclnando” <lfernando.div@gmail.com>
2024-12-15 20:47:10 -0300

    Feat/ Add some libraries & useState isLoading

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,16 +1,22 @@
+import { useEffect, useState } from "react";
 import { Alert, Text, View } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
 
+import { Loading } from "@/components/loading";
+
 import { api } from "@/services/api";
-import { useEffect } from "react";
 
 export default function Market() {
+  const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+
   const params = useLocalSearchParams<{ id: string }>()
 
   async function fetchMarket() {
     try {
       const { data } = await api.get(`/markets/${params.id}`)
-      console.log(data)
+      setData(data)
+      setIsLoading(false)
     } catch (error) {
       console.log(error)
       Alert.alert("Erro", "Não foi possível carregar os dados", [
@@ -25,6 +31,10 @@ export default function Market() {
   useEffect(() => {
     fetchMarket()
   },[params.id])
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center" }}>
