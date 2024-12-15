21a3bf206567d8b4b3ab9fa07e117a04c161c6de
“lfrclnando” <lfernando.div@gmail.com>
2024-12-15 20:39:49 -0300

    Feat/Add establishment data

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,8 +1,31 @@
-import { Text, View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { Alert, Text, View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+
+import { api } from "@/services/api";
+import { useEffect } from "react";
 
 export default function Market() {
   const params = useLocalSearchParams<{ id: string }>()
+
+  async function fetchMarket() {
+    try {
+      const { data } = await api.get(`/markets/${params.id}`)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Erro", "Não foi possível carregar os dados", [
+        {
+          text: "Ok",
+          onPress: () => router.back(),
+        },
+      ])
+    }
+  }
+
+  useEffect(() => {
+    fetchMarket()
+  },[params.id])
+
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>{params.id}</Text>
