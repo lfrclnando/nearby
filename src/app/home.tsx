838ad0272c918d56cfd1f6a141477bc27d58d6c1
“lfrclnando” <lfernando.div@gmail.com>
2024-12-14 21:05:43 -0300

    Feat/Applying categories in lists

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -7,11 +7,13 @@ import { Categories, CategoriesProps } from "@/components/categories";
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([])
+  const [category, setCategory] = useState("")
 
   async function fetchCategories() {
     try {
       const { data } = await api.get("/categories")
       setCategories(data)
+      setCategory(data[0].id)
     } catch (error) {
       console.log(error)
       Alert.alert("Categorias", "Não foi possível carregar as categorias.")
@@ -22,6 +24,6 @@ export default function Home() {
   }, [])
   return(
   <View style={{ flex: 1 }}>
-    <Categories data={categories} />
+    <Categories data={categories} onSelect={setCategory} selected={category} />
   </View>)
 }
