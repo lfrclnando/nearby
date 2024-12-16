8d30131e4d533df0eb82b8d5b3b5a66a1e27fbac
“lfrclnando” <lfernando.div@gmail.com>
2024-12-15 22:38:53 -0300

    Feat/ Add components for market, cover & details

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
-import { Alert, Text, View } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
+import { Alert, View } from "react-native";
+import { router, useLocalSearchParams, Redirect } from "expo-router";
 
 import { Loading } from "@/components/loading";
+import { Cover } from "@/components/market/cover";
+import { Details, PropsDetails } from "@/components/market/details";
 
 import { api } from "@/services/api";
 
+type DataProps = PropsDetails & {
+  cover: string
+}
+
 export default function Market() {
-  const [data, setData] = useState()
+  const [data, setData] = useState<DataProps>()
   const [isLoading, setIsLoading] = useState(true)
 
   const params = useLocalSearchParams<{ id: string }>()
@@ -36,9 +42,14 @@ export default function Market() {
     return <Loading />
   }
 
+  if (!data) {
+    return <Redirect href="/home" />
+  }
+
   return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{params.id}</Text>
+    <View style={{flex: 1 }} >
+     <Cover uri={data.cover} />
+     <Details data={data} /> 
     </View>
   );
 }
